e65c681e04c4345b5c0cf990bac746fea1e744dd
Olaitan Adedokun <kenzdex@gmail.com>
2025-07-11 08:09:38 +0100

    implement the job posting by creating a template and returning it

--- job_board/views.py
+++ job_board/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from job_board.models import JobPosting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h2>Hello to Job Board!</h2>')
+    non_active_postings = JobPosting.objects.filter(is_active=False)
+    context = {
+        "job_postings": non_active_postings
+    }
+    return render(request, "job_board/index.html", context)
